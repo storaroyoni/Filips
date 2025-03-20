fd9b264a2ad8aa3566d670ac26683bfa0d3815b4
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2025-03-20 21:25:23 +0200

    Deleted dummy methods and classes

--- server/healthServer/src/main/java/com/filips/healthServer/repository/RandRepo.java
+++
@@ -1,9 +0,0 @@
-package com.filips.healthServer.repository;
-
-import com.filips.healthServer.model.Mod;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RandRepo extends JpaRepository<Mod, Integer> {
-}
